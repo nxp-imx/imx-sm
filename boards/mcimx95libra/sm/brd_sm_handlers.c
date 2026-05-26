115dc1bbe4ac64a1fcfd7c1e0099d7070f16a83a
Benjamin Hahn <B.Hahn@phytec.de>
2026-05-26 13:51:29 +0200

    Add imx95libra board
    Add imx95libra board. This is only a copy from imx95evk board so far.
    Changes will be made in later commits to be able to see the changes
    needed for imx95libra better.
    
    Signed-off-by: Benjamin Hahn <B.Hahn@phytec.de>

---
+++ boards/mcimx95libra/sm/brd_sm_handlers.c
@@ -0,0 +1,372 @@
+/*
+** ###################################################################
+**
+** Copyright 2023-2025 NXP
+**
+** Redistribution and use in source and binary forms, with or without modification,
+** are permitted provided that the following conditions are met:
+**
+** o Redistributions of source code must retain the above copyright notice, this list
+**   of conditions and the following disclaimer.
+**
+** o Redistributions in binary form must reproduce the above copyright notice, this
+**   list of conditions and the following disclaimer in the documentation and/or
+**   other materials provided with the distribution.
+**
+** o Neither the name of the copyright holder nor the names of its
+**   contributors may be used to endorse or promote products derived from this
+**   software without specific prior written permission.
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+** ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+** WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+** DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+** ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+** (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+** LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+** ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+** SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**
+**
+** ###################################################################
+*/
+
+/*==========================================================================*/
+/* File containing the implementation of the handlers for the board.        */
+/*==========================================================================*/
+
+/* Includes */
+
+#include "sm.h"
+#include "brd_sm.h"
+#include "dev_sm.h"
+#include "fsl_lpi2c.h"
+#include "fsl_rgpio.h"
+
+/* Local defines */
+
+/* I2C device addresses */
+#define BOARD_PF09_DEV_ADDR         0x08U
+#define BOARD_PCAL6408A_DEV_ADDR    0x20U
+#define BOARD_PF5301_DEV_ADDR       0x2AU
+#define BOARD_PF5302_DEV_ADDR       0x29U
+#define BOARD_PCA2131_DEV_ADDR      0x53U
+
+#define PCAL6408A_INPUT_PF53_ARM_PG  1U
+#define PCAL6408A_INPUT_PF53_SOC_PG  2U
+#define PCAL6408A_INPUT_PF09_INT     3U
+#define PCAL6408A_INPUT_PCA2131_INT  6U
+
+/* Local types */
+
+/* Local variables */
+
+/* Global variables */
+
+PCAL6408A_Type g_pcal6408aDev;
+PF09_Type g_pf09Dev;
+PF53_Type g_pf5301Dev;
+PF53_Type g_pf5302Dev;
+PCA2131_Type g_pca2131Dev;
+
+irq_prio_info_t g_brdIrqPrioInfo[BOARD_NUM_IRQ_PRIO_IDX] =
+{
+    [BOARD_IRQ_PRIO_IDX_GPIO1_0] =
+    {
+        .irqId = GPIO1_0_IRQn,
+        .irqCntr = 0U,
+        .basePrio = 0U,
+        .dynPrioEn = false
+    }
+};
+
+bool g_pca2131Used = false;
+
+uint32_t g_pmicFaultFlags = 0U;
+
+/* Local functions */
+
+static void BRD_SM_Pf09Handler(void);
+
+/*--------------------------------------------------------------------------*/
+/* Init serial devices                                                      */
+/*--------------------------------------------------------------------------*/
+int32_t BRD_SM_SerialDevicesInit(void)
+{
+    int32_t status = SM_ERR_SUCCESS;
+    LPI2C_Type *const s_i2cBases[] = LPI2C_BASE_PTRS;
+    pcal6408a_config_t pcal6408Config;
+
+    /* Fill in PCAL6408A dev */
+    g_pcal6408aDev.i2cBase = s_i2cBases[BOARD_I2C_INSTANCE];
+    g_pcal6408aDev.devAddr = BOARD_PCAL6408A_DEV_ADDR;
+
+    /* Init the bus expander */
+    PCAL6408A_GetDefaultConfig(&pcal6408Config);
+    pcal6408Config.inputLatch = 0xFFU;
+    if (!PCAL6408A_Init(&g_pcal6408aDev, &pcal6408Config))
+    {
+        status = SM_ERR_HARDWARE_ERROR;
+    }
+    else
+    {
+        if (!PCAL6408A_IntMaskSet(&g_pcal6408aDev, PCAL6408A_INITIAL_MASK))
+        {
+            status = SM_ERR_HARDWARE_ERROR;
+        }
+    }
+
+    if (status == SM_ERR_SUCCESS)
+    {
+        /* Fill in PF09 PMIC handle */
+        g_pf09Dev.i2cBase = s_i2cBases[BOARD_I2C_INSTANCE];
+        g_pf09Dev.devAddr = BOARD_PF09_DEV_ADDR;
+        g_pf09Dev.crcEn = true;
+
+        /* Initialize PF09 PMIC */
+        if (!PF09_Init(&g_pf09Dev))
+        {
+            status = SM_ERR_HARDWARE_ERROR;
+        }
+
+        /* Disable voltage monitor 1 */
+        if (status == SM_ERR_SUCCESS)
+        {
+            if (!PF09_MonitorEnable(&g_pf09Dev, PF09_VMON1, false))
+            {
+                status = SM_ERR_HARDWARE_ERROR;
+            }
+        }
+
+        /* Disable voltage monitor 2 */
+        if (status == SM_ERR_SUCCESS)
+        {
+            if (!PF09_MonitorEnable(&g_pf09Dev, PF09_VMON2, false))
+            {
+                status = SM_ERR_HARDWARE_ERROR;
+            }
+        }
+
+        /* Disable the PWRUP interrupt */
+        if (status == SM_ERR_SUCCESS)
+        {
+            const uint8_t mask[PF09_MASK_LEN] =
+            {
+                [PF09_MASK_IDX_STATUS1] = 0x08U
+            };
+
+            if (!PF09_IntEnable(&g_pf09Dev, mask, PF09_MASK_LEN, false))
+            {
+                status = SM_ERR_HARDWARE_ERROR;
+            }
+        }
+
+        /* Change the LDO3 sequence */
+        if (status == SM_ERR_SUCCESS)
+        {
+            if (!PF09_PmicWrite(&g_pf09Dev, 0x4AU, 0x1EU, 0xFFU))
+            {
+                status = SM_ERR_HARDWARE_ERROR;
+            }
+        }
+
+        /* Set the LDO3 OV bypass */
+        if (status == SM_ERR_SUCCESS)
+        {
+            if (!PF09_PmicWrite(&g_pf09Dev, 0x7FU, 0xFCU, 0xFFU))
+            {
+                status = SM_ERR_HARDWARE_ERROR;
+            }
+        }
+
+        /* Enable the LDO3 in RUN mode */
+        if (status == SM_ERR_SUCCESS)
+        {
+            if (!PF09_PmicWrite(&g_pf09Dev, 0x7DU, 0x20U, 0xFFU))
+            {
+                status = SM_ERR_HARDWARE_ERROR;
+            }
+        }
+
+        /* Set the OV debounce to 50us due to errata ER011/12 */
+        if (status == SM_ERR_SUCCESS)
+        {
+            if (!PF09_PmicWrite(&g_pf09Dev, 0x37U, 0x94U, 0xFFU))
+            {
+                status = SM_ERR_HARDWARE_ERROR;
+            }
+        }
+
+        /* Save and clear any fault flags */
+        if (status == SM_ERR_SUCCESS)
+        {
+            if (!PF09_FaultFlags(&g_pf09Dev, &g_pmicFaultFlags, true))
+            {
+                status = SM_ERR_HARDWARE_ERROR;
+            }
+        }
+
+        /* Handle any already pending PF09 interrupts */
+        if (status == SM_ERR_SUCCESS)
+        {
+            BRD_SM_Pf09Handler();
+        }
+    }
+
+    if (status == SM_ERR_SUCCESS)
+    {
+        /* Fill in PF5301 PMIC handle */
+        g_pf5301Dev.i2cBase = s_i2cBases[BOARD_I2C_INSTANCE];
+        g_pf5301Dev.devAddr = BOARD_PF5301_DEV_ADDR;
+
+        /* Initialize PF5301 PMIC */
+        if (!PF53_Init(&g_pf5301Dev))
+        {
+            status = SM_ERR_HARDWARE_ERROR;
+        }
+
+    }
+
+    if (status == SM_ERR_SUCCESS)
+    {
+        /* Fill in PF5302 PMIC handle */
+        g_pf5302Dev.i2cBase = s_i2cBases[BOARD_I2C_INSTANCE];
+        g_pf5302Dev.devAddr = BOARD_PF5302_DEV_ADDR;
+
+        /* Initialize PF5302 PMIC */
+        if (!PF53_Init(&g_pf5302Dev))
+        {
+            status = SM_ERR_HARDWARE_ERROR;
+        }
+    }
+
+    if (status == SM_ERR_SUCCESS)
+    {
+        /* Fill in PCA2131 RTC handle */
+        g_pca2131Dev.i2cBase = s_i2cBases[BOARD_I2C_INSTANCE];
+        g_pca2131Dev.devAddr = BOARD_PCA2131_DEV_ADDR;
+
+        /* Initialize PCA2131 RTC */
+        if (!PCA2131_Init(&g_pca2131Dev))
+        {
+            status = SM_ERR_HARDWARE_ERROR;
+        }
+    }
+
+    if (status == SM_ERR_SUCCESS)
+    {
+        rgpio_pin_config_t gpioConfig =
+        {
+            kRGPIO_DigitalInput,
+            0U
+        };
+
+        /* Init GPIO1-10 */
+        RGPIO_PinInit(GPIO1, 10U, &gpioConfig);
+        RGPIO_SetPinInterruptConfig(GPIO1, 10U, kRGPIO_InterruptOutput0,
+            kRGPIO_InterruptLogicZero);
+    }
+
+    /* Return status */
+    return status;
+}
+
+/*--------------------------------------------------------------------------*/
+/* Set bus expander interrupt mask                                          */
+/*--------------------------------------------------------------------------*/
+int32_t BRD_SM_BusExpMaskSet(uint8_t val, uint8_t mask)
+{
+    int32_t status = SM_ERR_SUCCESS;
+    static uint8_t cachedMask = PCAL6408A_INITIAL_MASK;
+    uint8_t newMask = (cachedMask & ~mask);
+
+    newMask |= val;
+
+    /* Mask changed? */
+    if (cachedMask != newMask)
+    {
+        if (PCAL6408A_IntMaskSet(&g_pcal6408aDev, newMask))
+        {
+            cachedMask = newMask;
+        }
+        else
+        {
+            status = SM_ERR_HARDWARE_ERROR;
+        }
+    }
+
+    /* Return status */
+    return status;
+}
+
+/*--------------------------------------------------------------------------*/
+/* GPIO1 handler                                                            */
+/*--------------------------------------------------------------------------*/
+void GPIO1_0_IRQHandler(void)
+{
+    uint32_t flags;
+    uint8_t status, val;
+
+    /* Get GPIO status */
+    flags = RGPIO_GetPinsInterruptFlags(GPIO1, kRGPIO_InterruptOutput0);
+
+    /* Get PCAL6408A status */
+    (void) PCAL6408A_IntStatusGet(&g_pcal6408aDev, &status);
+
+    /* Get value and Clear PCAL6408A interrupts */
+    (void) PCAL6408A_InputGet(&g_pcal6408aDev, &val);
+
+    /* Clear GPIO interrupts */
+    RGPIO_ClearPinsInterruptFlags(GPIO1, kRGPIO_InterruptOutput0, flags);
+
+    /* Handle PF09 interrupt */
+    if ((status & BIT8(PCAL6408A_INPUT_PF09_INT)) != 0U)
+    {
+        /* Asserts low */
+        if ((val & BIT8(PCAL6408A_INPUT_PF09_INT)) == 0U)
+        {
+            BRD_SM_Pf09Handler();
+        }
+    }
+
+    /* Handle PCA2131 interrupt */
+    if (g_pca2131Used && ((status & BIT8(PCAL6408A_INPUT_PCA2131_INT))
+        != 0U))
+    {
+        /* Asserts low */
+        if ((val & BIT8(PCAL6408A_INPUT_PCA2131_INT)) == 0U)
+        {
+            BRD_SM_BbmHandler();
+        }
+    }
+
+    /* Handle controls interrupts */
+    BRD_SM_ControlHandler(status, val);
+
+    /* Adjust dynamic IRQ priority */
+    (void) DEV_SM_IrqPrioUpdate();
+}
+
+/*==========================================================================*/
+
+/*--------------------------------------------------------------------------*/
+/* PF09 handler                                                             */
+/*--------------------------------------------------------------------------*/
+static void BRD_SM_Pf09Handler(void)
+{
+    uint8_t stat[PF09_MASK_LEN] = { 0 };
+
+    /* Read status of interrupts */
+    (void) PF09_IntStatus(&g_pf09Dev, stat, PF09_MASK_LEN);
+
+    /* Clear pending */
+    (void) PF09_IntClear(&g_pf09Dev, stat, PF09_MASK_LEN);
+
+    /* Handle pending temp interrupts */
+    if ((stat[PF09_MASK_IDX_STATUS2] & 0x0FU) != 0U)
+    {
+        BRD_SM_SensorHandler();
+    }
+}
+
---
+++ boards/mcimx95libra/sm/brd_sm_sensor.h
@@ -0,0 +1,210 @@
+/*
+** ###################################################################
+**
+** Copyright 2023 NXP
+**
+** Redistribution and use in source and binary forms, with or without modification,
+** are permitted provided that the following conditions are met:
+**
+** o Redistributions of source code must retain the above copyright notice, this list
+**   of conditions and the following disclaimer.
+**
+** o Redistributions in binary form must reproduce the above copyright notice, this
+**   list of conditions and the following disclaimer in the documentation and/or
+**   other materials provided with the distribution.
+**
+** o Neither the name of the copyright holder nor the names of its
+**   contributors may be used to endorse or promote products derived from this
+**   software without specific prior written permission.
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+** ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+** WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+** DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+** ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+** (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+** LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+** ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+** SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**
+**
+** ###################################################################
+*/
+
+/*==========================================================================*/
+/*!
+ * @addtogroup BRD_SM_MX95EVK
+ * @{
+ *
+ * @file
+ * @brief
+ *
+ * Header file containing the SM API for the board sensors.
+ */
+/*==========================================================================*/
+
+#ifndef BRD_SM_SENSOR_H
+#define BRD_SM_SENSOR_H
+
+/* Includes */
+
+#include "sm.h"
+#include "dev_sm.h"
+
+/* Defines */
+
+/*!
+ * @name Board redirection defines
+ * @{
+ */
+#define SM_SENSORNAMEGET       BRD_SM_SensorNameGet       /*!< Sensor name */
+#define SM_SENSORDESCRIBE      BRD_SM_SensorDescribe      /*!< Sensor describe */
+#define SM_SENSORREADINGGET    BRD_SM_SensorReadingGet    /*!< Sensor read */
+#define SM_SENSORTRIPPOINTSET  BRD_SM_SensorTripPointSet  /*!< Sensor trip point */
+#define SM_SENSORENABLE        BRD_SM_SensorEnable        /*!< Sensor enable */
+#define SM_SENSORISENABLED     BRD_SM_SensorIsEnabled     /*!< Sensor status */
+/** @} */
+
+/*! Number of board sensors */
+#define BRD_SM_NUM_SENSOR  3UL
+
+/*! Total number of sensors */
+#define SM_NUM_SENSOR  (DEV_SM_NUM_SENSOR + BRD_SM_NUM_SENSOR)
+
+/*!
+ * @name BRD_SM sensor domain indexes
+ */
+/** @{ */
+#define BRD_SM_SENSOR_TEMP_PF09    (DEV_SM_NUM_SENSOR + 0U)  /*!< PF09 temp sensor */
+#define BRD_SM_SENSOR_TEMP_PF5302  (DEV_SM_NUM_SENSOR + 1U)  /*!< PF5302 temp sensor */
+#define BRD_SM_SENSOR_TEMP_PF5301  (DEV_SM_NUM_SENSOR + 2U)  /*!< PF5301 temp sensor */
+/** @} */
+
+/* Types */
+
+/* Functions */
+
+/*!
+ * @name Board sensor functions
+ * @{
+ */
+
+/*!
+ * Get a board sensor name.
+ *
+ * @param[in]     sensorId        Sensor name to get
+ * @param[out]    sensorNameAddr  Return pointer to name
+ * @param[out]    len             Return max length of all sensor names
+ *
+ * This function allows the caller to get the name of a sensor.
+ *
+ * @return Returns the status (::SM_ERR_SUCCESS = success).
+ *
+ * Return errors (see @ref STATUS "SM error codes"):
+ * - ::SM_ERR_NOT_FOUND: if \a sensorId is invalid.
+ */
+int32_t BRD_SM_SensorNameGet(uint32_t sensorId, string *sensorNameAddr,
+    int32_t *len);
+
+/*!
+ * Get a board sensor description.
+ *
+ * @param[in]     sensorId   Sensor description to get
+ * @param[out]    desc       Return pointer to the description
+ *
+ * This function allows the caller to get the description of a sensor.
+ *
+ * @return Returns the status (::SM_ERR_SUCCESS = success).
+ *
+ * Return errors (see @ref STATUS "SM error codes"):
+ * - ::SM_ERR_NOT_FOUND: if \a sensorId is invalid.
+ */
+int32_t BRD_SM_SensorDescribe(uint32_t sensorId,
+    dev_sm_sensor_desc_t *desc);
+
+/*!
+ * Read a board sensor.
+ *
+ * @param[in]     sensorId         Sensor to read
+ * @param[out]    sensorValue      Return pointer to sensor value
+ * @param[out]    sensorTimestamp  Return pointer to timestamp
+ *
+ * This function allows the caller to read the value of a sensor.
+ *
+ * @return Returns the status (::SM_ERR_SUCCESS = success).
+ *
+ * Return errors (see @ref STATUS "SM error codes"):
+ * - ::SM_ERR_NOT_FOUND: if \a sensorId is invalid.
+ */
+int32_t BRD_SM_SensorReadingGet(uint32_t sensorId, int64_t *sensorValue,
+    uint64_t *sensorTimestamp);
+
+/*!
+ * Configure a trip point for a board sensor.
+ *
+ * @param[in]     sensorId      Sensor to configure TP
+ * @param[in]     tripPoint     Trip point ID
+ * @param[in]     value         Value to trip at
+ * @param[in]     eventControl  Event control (notifications)
+ *
+ * This function allows the caller to configure one of the trip
+ * points for a sensor. The caller can also configure which crossing
+ * direction will generate a notification.
+ *
+ * @return Returns the status (::SM_ERR_SUCCESS = success).
+ *
+ * Return errors (see @ref STATUS "SM error codes"):
+ * - ::SM_ERR_NOT_FOUND: if \a sensorId is invalid.
+ */
+int32_t BRD_SM_SensorTripPointSet(uint32_t sensorId, uint8_t tripPoint,
+    int64_t value, uint8_t eventControl);
+
+/*!
+ * Enable/disable a board sensor.
+ *
+ * @param[in]     sensorId            Sensor to enable/disable
+ * @param[in]     enable              True to enable
+ * @param[in]     timestampReporting  True to enable timestamp reporting
+ *
+ * This function allows the caller to enable/disable a sensor.
+ *
+ * @return Returns the status (::SM_ERR_SUCCESS = success).
+ *
+ * Return errors (see @ref STATUS "SM error codes"):
+ * - ::SM_ERR_NOT_FOUND: if \a sensorId is invalid.
+ */
+int32_t BRD_SM_SensorEnable(uint32_t sensorId, bool enable,
+    bool timestampReporting);
+
+/*!
+ * Get enable/disable state of a board sensor.
+ *
+ * @param[in]     sensorId            Sensor to get state
+ * @param[out]    enabled             Return pointer to enable
+ * @param[out]    timestampReporting  Return pointer to reporting
+ *
+ * This function allows the caller to get the enable/disable
+ * state of a sensor.
+ *
+ * @return Returns the status (::SM_ERR_SUCCESS = success).
+ *
+ * Return errors (see @ref STATUS "SM error codes"):
+ * - ::SM_ERR_NOT_FOUND: if \a sensorId is invalid.
+ */
+int32_t BRD_SM_SensorIsEnabled(uint32_t sensorId, bool *enabled,
+    bool *timestampReporting);
+
+/*!
+ * Sensor interrupt handler.
+ *
+ * Called by PF09 interrupt handler.
+ */
+void BRD_SM_SensorHandler(void);
+
+/** @} */
+
+#endif /* BRD_SM_SENSOR_H */
+
+/** @} */
+
